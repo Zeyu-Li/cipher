9b1f7bff3105d5e72ce22cb8099986bf6a4fb189
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-06-18 19:29:05 -0600

    finished cipher

--- cipher.py
+++ cipher.py
@@ -1,73 +1,80 @@
-# Cipher by Andrew Li
-# Creates a cipher (public and private key pair)
+# Feistel Cipher by Andrew Li
+# Creates a cipher based on the Feistel cipher (public and private key pair)
 import secrets
+import math
 
-# you can change the number of rounds the cipher goes
-ROUNDS = 8
 
-def decode(final_hash):
+def code(final_hash, key, rounds):
+    # both encodes and decodes
     hash_len = len(final_hash)
 
-    first_half = int(final_hash[:(hash_len+1)//2], 16)
-    second_half = int(final_hash[(hash_len+1)//2:], 16)
+    first_half = int(final_hash[:-int(hash_len/2)], 16)
+    second_half = int(final_hash[-int(hash_len/2):], 16)
 
     # decrypt
-    for _ in range(ROUNDS):
+    for _ in range(rounds):
 
-        second_half = first_half ^ second_half
+        second_half, key = hash_function(second_half, key)
+        first_half = first_half ^ second_half
+
         # switch
         first_half, second_half = second_half, first_half
 
-    original_hex = str(hex(second_half)).lstrip("0x") + str(hex(first_half)).lstrip("0x")
+    first_part = str(hex(second_half)).lstrip("0x") 
+    second_part = str(hex(first_half)).lstrip("0x")
 
-    return original_hex
+    # sometimes 0 is omitted so watch out
+    # there may be more bugs here relating to this :(
+    while len(first_part) != int(hash_len/2):
+        first_part += "0"
 
-def to_string(hex):
+    while len(second_part) != int(hash_len/2):
+        second_part = "0" + second_part
+
+    return first_part + second_part, key
+
+def hash_function(hash, key):
+    # takes a hex and transforms it
+    # If you want to change the hash function, you can do it here
+
+    # hash = hash ^ key
+
+    return hash, key
+
+def to_string(some_hex):
     # turns hex into string
-    return bytes.fromhex(hex).decode("ASCII")
+
+    return bytes.fromhex(some_hex).decode("utf-8")
 
 def main():
-    msg = "Hello World!"
-    pub_key = pvt_key = ""
-    empty = "\0"
+    # predefined static message
+    msg = "Hello World"
 
-    # msg to hex and pad it out to make even
+    # you can change the number of rounds the cipher goes
+    ROUNDS = 10
+    # uncomment if you want message to be inputted
+    # msg = str(input("Secret message: "))
+    pub_key = pvt_key = ""
+
+    # random number
+    pvt_key = int(secrets.token_hex(8).encode('utf-8').hex(), 16)
 
     # msg to hex
     pub_key = msg.encode('utf-8').hex()
-    msg_len = len(pub_key)
 
-
-    if msg_len % 2 != 0:
-        pub_key += empty.encode('utf-8').hex()
-
-    # random number
-    # pvt_key = secrets.token_hex(16)
-
-    # divides into 2 halves
-    first_half = int(pub_key[:(msg_len+1)//2], 16)
-    second_half = int(pub_key[(msg_len+1)//2:], 16)
-
-    # encrypt
-    for _ in range(ROUNDS):
-
-        second_half = first_half ^ second_half
-        first_half, second_half = second_half, first_half
-
-    # concat at the end
-    final_hash = str(hex(second_half)).lstrip("0x") + str(hex(first_half)).lstrip("0x")
+    final_hash, new_key = code(pub_key, pvt_key, ROUNDS)
 
     # debug
-    print(final_hash)
+    # print(final_hash)
 
     # print
     # print(f"The public key is: {pub_key} ")
     # print(f"The private key is: {pvt_key} ")
 
     # decode
-    original_hex = decode(final_hash)
+    original_hex, original_key = code(final_hash, new_key, ROUNDS)
 
-    print(to_string(original_hex))
+    print(f"The secret message was: {to_string(original_hex)}")
 
 
 if __name__ == "__main__":
